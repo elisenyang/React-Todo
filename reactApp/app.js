ce3be3ffbc04f2c705b937b5d86b3c1e8137fcd4
elisenyang <elisey@seas.upenn.edu>
2017-06-30 15:05:03 -0700

    staticapp

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>HELLOOOOOO</p>, document.getElementById('root'))
+
+const dummyData = ["eat", "code", "sleep"]
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <li><button type="button">X</button>{this.props.task}</li>
+    )
+  }
+}
+
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <ul>
+        {dummyData.map((task)=><Todo task={task}/>)}
+      </ul>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <input type="text" placeholder="Todo Item"/>
+        <input type="submit" value="Add"/>
+      </div>
+    )
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <InputLine/>
+        <TodoList/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp/>, document.getElementById('root'))
